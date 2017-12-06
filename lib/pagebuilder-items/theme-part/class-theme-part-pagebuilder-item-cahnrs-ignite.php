721b48fc4959eb66b269ade563dd3d58337af87b
danialbleile <danialbleile@wsu.edu>
2017-12-06 14:59:33 -0800

    Added drop down to theme part

--- lib/pagebuilder-items/theme-part/class-theme-part-pagebuilder-item-cahnrs-ignite.php
+++ lib/pagebuilder-items/theme-part/class-theme-part-pagebuilder-item-cahnrs-ignite.php
@@ -19,9 +19,29 @@ class Theme_Part_Pagebuilder_Item_CAHNRS_Ignite  extends CPB_Item {
 	
 	
 	public function form( $settings , $content ){
+		
+		$args = array(
+			'posts_per_page'   => -1,
+			'orderby'          => 'date',
+			'order'            => 'DESC',
+			'post_type'        => 'theme_part',
+			'post_status'      => 'publish',
+		);
+		
+		$post_options = array();
+		
+		$posts_array = get_posts( $args );
+		
+		foreach( $posts_array as $tp_post ){
+			
+			$post_options[ $tp_post->ID ] = $tp_post->post_title;
+			
+		} // End foreach
+		
+		$html = $this->form_fields->select_field( $this->get_input_name('part_id'), $settings['part_id'], $post_options, 'Theme Part' );
 
 		
-		$html = $this->form_fields->text_field( $this->get_input_name('part_id') , $settings['part_id'] , 'Part ID:' );
+		//$html = $this->form_fields->text_field( $this->get_input_name('part_id') , $settings['part_id'] , 'Part ID:' );
 		
 		
 		return array('Basic' => $html  );
